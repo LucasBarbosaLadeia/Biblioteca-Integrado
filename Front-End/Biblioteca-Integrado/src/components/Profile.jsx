fe055255434ae14d9d975aa537cb30e835859cfe
Gabrielhss06 <161782014+Gabrielhss06@users.noreply.github.com>
2025-08-27 16:02:45 -0300

    Terminei a tela login e home

---
+++ Front-End/Biblioteca-Integrado/src/components/Profile.jsx
@@ -0,0 +1,69 @@
+import React from "react";
+import { View, StyleSheet } from "react-native";
+import { FontAwesome } from "@expo/vector-icons";
+import Svg, { Circle } from "react-native-svg";
+
+const UserAvatar = () => {
+  const radius = 30;
+  const strokeWidth = 6;
+  const circumference = 2 * Math.PI * radius;
+
+  return (
+    <View style={styles.container}>
+      <Svg height="80" width="80" style={styles.svg}>
+        {/* Círculo base lilás */}
+        <Circle
+          cx="40"
+          cy="40"
+          r={radius}
+          stroke="#fafafaff"
+          strokeWidth={strokeWidth}
+          strokeDasharray={`${circumference * 0.7} ${circumference * 0.3}`} // 73% traço, 27% espaço
+          strokeDashoffset={10}
+          strokeLinecap="round"
+          transform="rotate(19, 40, 40)"
+          fill="none"
+        />
+
+        {/* Traço decorativo roxo em 30° */}
+        <Circle
+          cx="40"
+          cy="40"
+          r={radius}
+          stroke="#7c3aed"
+          strokeWidth={strokeWidth}
+          fill="none"
+          strokeDasharray={`${circumference * 0.15} ${circumference * 0.85}`}
+          strokeDashoffset={10} // controla o espaço inicial
+          strokeLinecap="round"
+          transform="rotate(-60, 40, 40)"
+        />
+      </Svg>
+
+      {/* Ícone central */}
+      <View style={styles.iconContainer}>
+        <FontAwesome name="user" size={32} color="#7c3aed" />
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  svg: {
+    position: "absolute",
+  },
+  iconContainer: {
+    width: 45,
+    height: 45,
+    borderRadius: 30,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "rgba(248, 242, 242, 0.73)",
+  },
+});
+
+export default UserAvatar;
--- Front-End/Biblioteca-Integrado/src/screens/HomeScreen.jsx
+++ Front-End/Biblioteca-Integrado/src/screens/HomeScreen.jsx
@@ -1,23 +1,115 @@
 import React from "react";
-import { View, Text, StyleSheet, Button } from "react-native";
+import {
+  View,
+  StyleSheet,
+  SafeAreaView,
+  ImageBackground,
+  Text,
+} from "react-native";
+
+// Importe os componentes que criamos
+import MenuButton from "../components/Botao";
+import ProfileHeader from "../components/Profile";
+import TabBar from "../components/TagBar";
+
+// Importe a imagem de fundo
+import BackgroundImage from "../assets/background.png";
+
+const user = {
+  name: "João Silva",
+};
 
 const HomeScreen = ({ navigation }) => {
+  const handPesquisar = () => {
+    navigation.navigate("Pesquisa");
+  };
+  const handMeusLivros = () => {
+    console.log("Meus Livros");
+  };
+  const handMeusDados = () => {
+    console.log("Meus Dados");
+  };
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Bem-vindo(a)!</Text>
-      <Button title="Voltar para o Login" onPress={() => navigation.goBack()} />
-    </View>
+    <ImageBackground source={BackgroundImage} style={styles.backgroundImage}>
+      <SafeAreaView style={styles.safeArea}>
+        <View style={styles.container}>
+          {/* 1. Cabeçalho */}
+          <View style={styles.headerContainer}>
+            <View style={styles.profileRow}>
+              <ProfileHeader />
+              <Text style={styles.userName}>Olá, {user.name}</Text>
+            </View>
+          </View>
+          {/* 2. Menu Principal */}
+          <View style={styles.menuContainer}>
+            <MenuButton
+              title="Buscar Livros"
+              iconName="search"
+              onPress={() => handPesquisar("Pesquisa")}
+              style={{ marginBottom: 30 }}
+            />
+            <MenuButton
+              title="Meus Livros"
+              iconName="book"
+              onPress={() => console.log("Meus Livros")}
+              style={{ marginBottom: 30 }}
+            />
+            <MenuButton
+              title="Meus Dados"
+              iconName="profile"
+              onPress={() => console.log("Meus Dados")}
+              style={{ marginBottom: 330 }}
+            />
+          </View>
+        </View>
+        {/* 3. Rodapé */}
+        <TabBar />
+      </SafeAreaView>
+    </ImageBackground>
   );
 };
 
 const styles = StyleSheet.create({
-  container: {
+  backgroundImage: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "#F5F5F5",
+    resizeMode: "cover",
   },
-  title: { fontSize: 28, fontWeight: "bold", marginBottom: 10 },
+  safeArea: {
+    flex: 1,
+  },
+  container: {
+    flex: 30,
+    paddingHorizontal: 20,
+    alignItems: "center",
+    marginTop: 50,
+  },
+  headerContainer: {
+    width: "90%",
+    flexDirection: "row", // Adicione esta linha
+    alignItems: "center",
+    marginBottom: 20,
+    marginTop: 30,
+  },
+  profileRow: {
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 25,
+  },
+  userName: {
+    fontSize: 24,
+    fontWeight: "bold",
+    color: "#FFFFFF",
+  },
+  menuContainer: {
+    width: "100%",
+    marginTop: 20,
+    padding: 20,
+    backgroundColor: "rgba(0, 0, 0, 0.2)",
+    borderRadius: 20,
+    borderWidth: 1,
+    borderColor: "rgba(255, 255, 255, 0.5)",
+  },
 });
 
 export default HomeScreen;
