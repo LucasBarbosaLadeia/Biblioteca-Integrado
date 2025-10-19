19d59ccf586f318585d6b986db30e3da0142601c
Gabrielhss06 <161782014+Gabrielhss06@users.noreply.github.com>
2025-10-18 23:25:36 -0300

    Coloquei a tela de logim em tailwind e ta funcionando na web e no mobile

--- Front-End/Biblioteca-Integrado/src/screens/LoginScreen.jsx
+++ Front-End/Biblioteca-Integrado/src/screens/LoginScreen.jsx
@@ -58,6 +58,7 @@ const LoginScreen = ({ navigation }) => {
     <ImageBackground
       source={BackgroundImage}
       resizeMode="cover"
+      // CLASSE NATIVEWIND APLICADA AQUI
       className="flex-1"
       style={
         Platform.OS === "web"
@@ -67,6 +68,7 @@ const LoginScreen = ({ navigation }) => {
     >
       <SafeAreaView className="flex-1">
         <KeyboardAvoidingView
+          // CLASSE NATIVEWIND APLICADA AQUI
           className="flex-1 justify-center items-center p-5"
           behavior={Platform.OS === "ios" ? "padding" : "height"}
         >
@@ -84,12 +86,19 @@ const LoginScreen = ({ navigation }) => {
             message={errorMessage}
             buttonText={"OK"}
           />
-          <Text className="text-3xl md:text-4xl font-bold text-gray-200 mb-12">
+          <Text
+            // CLASSE NATIVEWIND E RESPONSIVIDADE APLICADA AQUI
+            className="text-3xl md:text-4xl font-bold text-gray-200 mb-12"
+          >
             Biblioteca Integrado
           </Text>
 
-          <View className="w-11/12 md:w-2/3 lg:w-1/3">
+          <View
+            // CLASSE NATIVEWIND E RESPONSIVIDADE APLICADA AQUI
+            className="w-11/12 md:w-2/3 lg:w-1/3"
+          >
             <TextInput
+              // CLASSE NATIVEWIND APLICADA AQUI
               className="h-12 bg-white rounded-lg px-4 text-base text-gray-800 mb-4"
               placeholder="Registro (RA)"
               placeholderTextColor="#BBBBBB"
@@ -98,8 +107,12 @@ const LoginScreen = ({ navigation }) => {
               onChangeText={(text) => setRa(maskRA(text))}
             />
 
-            <View className="flex-row items-center bg-white rounded-lg h-12 px-1 mb-4">
+            <View
+              // CLASSE NATIVEWIND APLICADA AQUI
+              className="flex-row items-center bg-white rounded-lg h-12 px-1 mb-4"
+            >
               <TextInput
+                // CLASSE NATIVEWIND APLICADA AQUI
                 className="flex-1 h-12 px-3 text-base text-gray-800 bg-transparent"
                 placeholder="Senha"
                 placeholderTextColor="#BBBBBB"
@@ -120,14 +133,23 @@ const LoginScreen = ({ navigation }) => {
             </View>
 
             <TouchableOpacity
+              // CLASSE NATIVEWIND APLICADA AQUI
               className="h-12 bg-black rounded-lg justify-center items-center mt-2 mb-5"
               onPress={handleLogin}
             >
-              <Text className="text-white text-lg font-bold">Entrar</Text>
+              <Text
+                // CLASSE NATIVEWIND APLICADA AQUI
+                className="text-white text-lg font-bold"
+              >
+                Entrar
+              </Text>
             </TouchableOpacity>
 
             <TouchableOpacity onPress={() => setShowEsqueceuSenha(true)}>
-              <Text className="text-gray-200 text-sm text-center">
+              <Text
+                // CLASSE NATIVEWIND APLICADA AQUI
+                className="text-gray-200 text-sm text-center"
+              >
                 Esqueceu a senha?
               </Text>
             </TouchableOpacity>
